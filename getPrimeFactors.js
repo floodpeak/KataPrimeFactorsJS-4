9170b639a334e9bf42dde1462608961121da38f1
floodpeak <hongfengzhou@yahoo.com>
2016-12-05 21:52:58 +0800

    rf, combine the logic of 8 with logic ahead

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,10 +1,11 @@
 module.exports = function(number){
-  if(number === 8){
-    return [2,2,2]
-  }
   var results = []
   var base = 2
   if(number % base === 0){
+    results.push(base)
+    number /= base
+  }
+  if(number === 4){
     results.push(base)
     number /= base
   }
