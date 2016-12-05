dd2873102b064be9d285037ea69a908fed1fc99b
floodpeak <hongfengzhou@yahoo.com>
2016-12-05 22:05:33 +0800

    rf, combine the logic of greater than 1 into the loop

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,11 +1,8 @@
 module.exports = function(number){
   var results = []
 
-  for(var base = 2;base < number;base++)
+  for(var base = 2;base <= number;base++)
     for(;number % base === 0;number /= base)
       results.push(base)
-  if(number > 1){
-    results.push(number)
-  }
   return results
 }
