2082fa7baf023527679e5063e5439a3564583c21
floodpeak <hongfengzhou@yahoo.com>
2016-12-05 22:03:20 +0800

    rf, replace the while loop with for loop

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -2,9 +2,8 @@ module.exports = function(number){
   var results = []
 
   for(var base = 2;base < number;base++){
-    while(number % base === 0){
+    for(;number % base === 0;number /= base){
       results.push(base)
-      number /= base
     }
   }
   if(number > 1){
