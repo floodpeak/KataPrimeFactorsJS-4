d30b15666ecdee6528dfeaa566ba6ce7c0daa361
floodpeak <hongfengzhou@yahoo.com>
2016-12-05 21:35:47 +0800

    rf, combine the logic of 4,6

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,8 +1,5 @@
 module.exports = function(number){
-  if(number === 6){
-    return [2,number/2]
-  }
-  if(number === 4){
+  if(number === 4 || number === 6){
     return [2,number/2]
   }
   if(number > 1){
