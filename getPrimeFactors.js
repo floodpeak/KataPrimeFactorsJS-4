18457ae390b97d9914b30f9b71a2c46133079862
floodpeak <hongfengzhou@yahoo.com>
2016-12-05 21:34:57 +0800

    rf, make the logic the same for 4,6

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,9 +1,9 @@
 module.exports = function(number){
   if(number === 6){
-    return [2,3]
+    return [2,number/2]
   }
   if(number === 4){
-    return [2,2]
+    return [2,number/2]
   }
   if(number > 1){
     return [number]
