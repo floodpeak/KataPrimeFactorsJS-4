ea73c176f8433858c70e1d123e0ad7ee4b0ed410
floodpeak <hongfengzhou@yahoo.com>
2016-12-05 21:49:05 +0800

    rf, pull out the magic number 2

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,8 +1,9 @@
 module.exports = function(number){
   var results = []
-  if(number % 2 === 0 && number > 2){
-    results.push(2)
-    number /= 2
+  var base = 2
+  if(number % base === 0){
+    results.push(base)
+    number /= base
   }
   if(number > 1){
     results.push(number)
