65c681804c8004df7e62391f24044d46eac81f6b
floodpeak <hongfengzhou@yahoo.com>
2016-12-05 21:56:43 +0800

    rf, change the place of logic for 9

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,12 +1,12 @@
 module.exports = function(number){
-  if(number === 9){
-    return [3,3]
-  }
   var results = []
   var base = 2
   while(number % base === 0){
     results.push(base)
     number /= base
+  }
+  if(number === 9){
+    return [3,3]
   }
   if(number > 1){
     results.push(number)
