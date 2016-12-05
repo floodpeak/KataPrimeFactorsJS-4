0465eeb3f4cb8aed935ee206f575c2bec0eb9b09
floodpeak <hongfengzhou@yahoo.com>
2016-12-05 22:03:44 +0800

    rf, remove unnecessary brace

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,11 +1,9 @@
 module.exports = function(number){
   var results = []
 
-  for(var base = 2;base < number;base++){
-    for(;number % base === 0;number /= base){
+  for(var base = 2;base < number;base++)
+    for(;number % base === 0;number /= base)
       results.push(base)
-    }
-  }
   if(number > 1){
     results.push(number)
   }
